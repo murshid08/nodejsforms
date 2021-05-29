b602a7ae3ff0b022c1015dd9da4745428289ca23
murshid veypey <muchimurshid.790@gamil.com>
2021-05-29 15:53:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const handlebars=require('express-handlebars')
+app.set('view engine','handlebars')
+app.engine('handlebars',handlebars({
+    layoutsDir:__dirname+'/views/layouts',
+}))
+app.use(express.static('public'))
+app.get('/', (req, res) => res.render('main',{layout:'index'}));
+
+//Makes the app listen to port 3000
+app.listen(port, () => console.log(`App listening to port ${port}`));
